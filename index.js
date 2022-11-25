20a316c97368afca194da2a9a6a13a9857450ddb
Artyom503 <78357397+Artyom503@users.noreply.github.com>
2022-11-25 13:57:13 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const TeleBot = require('telebot');
 
-const bot = new TeleBot(5800302294:AAG2KLKfgYOb4vlEt3dMAfY2LPvL4jYMqJ0);
+const bot = new TeleBot('5800302294:AAG2KLKfgYOb4vlEt3dMAfY2LPvL4jYMqJ0');
 bot.on(['/start', '/hello'], (msg) => msg.reply.text('Welcome!'));
 bot.on('sticker', (msg) => {
     return msg.reply.sticker('http://i.imgur.com/VRYdhuD.png', { asReply: true });
